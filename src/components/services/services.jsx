d29c99ca09f2c427945f70e3ddcb1f1575b84b10
Muhammad Zawar <mzawar189@gmail.com>
2024-12-25 23:33:32 +0500

    add font

--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -43,7 +43,7 @@ const ServicesSection = () => {
             alt="International And Domestic Flight Tickets"
           />
           <div className="flex flex-col gap-y-3 text-mid-night-blue">
-            <span className="text-4xl lg:text-5xl rtl:font-bold xl:text-[60px] font-montserrat rtl:font-[kufam] rtl:text-golden-dark/60 text-[#bd9c0080] font-[General]">
+            <span className="text-4xl lg:text-5xl rtl:font-bold xl:text-[60px] font-[montserrat] rtl:font-[kufam] rtl:text-golden-dark/60 text-[#bd9c0080] ">
               01
             </span>
             <div className="flex flex-col text-2xl lg:text-[30px] font-bold rtl:text-xl rtl:lg:text-[28px] xl:text-[36px] ltr:font-[cooper] rtl:font-kufam rtl:leading-9 lg:gap-y-4">
@@ -74,8 +74,191 @@ const ServicesSection = () => {
             alt="Hotel Reservation"
           />
           <div className="flex flex-col gap-y-3 text-mid-night-blue ">
-            <span className="text-4xl lg:text-5xl rtl:font-bold xl:text-[60px] font-montserrat rtl:font-[kufam] rtl:text-golden-dark/60 text-[#bd9c0080] font-[General]">
+            <span className="text-4xl lg:text-5xl rtl:font-bold xl:text-[60px] font-[montserrat] rtl:font-[kufam] rtl:text-golden-dark/60 text-[#bd9c0080] font-[General]">
               02
+            </span>
+            <div className="flex flex-col text-2xl lg:text-[30px] font-bold rtl:text-xl rtl:lg:text-[28px] xl:text-[36px] ltr:font-[cooper] rtl:font-kufam rtl:leading-9 lg:gap-y-4">
+              <h1>
+                <FormattedMessage id="servicesBox2Heading1" />
+              </h1>
+              <h1>
+                <FormattedMessage id="servicesBox2Heading2" />
+              </h1>
+            </div>
+            <p className="ltr:font-[gabriola] rtl:font-kufam text-xl xl:text-[22px] rtl:text-base rtl:sm:text-lg rtl:xl:text-lg leading-5 ltr:lg:!leading-9 rtl:leading-10 rtl:lg:!leading-loose">
+              <FormattedMessage id="servicesBoxDescription" />
+            </p>
+            <button
+              type="button"
+              className="primary-btn bg-[#daa520] text-white px-5 py-2 text-lg rounded font-gabriola w-[139px] h-[46px] hover:bg-[#b8860b]"
+            >
+              <FormattedMessage id="servicesBoxButton" />
+            </button>
+          </div>
+        </div>
+        {/* Service 3 */}
+        <div className="flex items-start md:items-center flex-col md:flex-row gap-y-6 sm:gap-y-12 md:gap-x-12 xl:gap-x-32">
+          <img
+            className="w-[300px] h-[300px] sm:w-[500px] sm:h-[420px] md:w-[300px] md:h-[420px] lg:w-[400px] lg:h-[400px] xl:w-[450px] xl:h-[450px] !rounded-2xl object-contain sm:object-cover lg:object-contain"
+            src={services1}
+            alt="International And Domestic Flight Tickets"
+          />
+          <div className="flex flex-col gap-y-3 text-mid-night-blue">
+            <span className="text-4xl lg:text-5xl rtl:font-bold xl:text-[60px] font-[montserrat] rtl:font-[kufam] rtl:text-golden-dark/60 text-[#bd9c0080] ">
+              03
+            </span>
+            <div className="flex flex-col text-2xl lg:text-[30px] font-bold rtl:text-xl rtl:lg:text-[28px] xl:text-[36px] ltr:font-[cooper] rtl:font-kufam rtl:leading-9 lg:gap-y-4">
+              <h1>
+                <FormattedMessage id="servicesBox1Heading1" />
+              </h1>
+              <h1>
+                <FormattedMessage id="servicesBox1Heading2" />
+              </h1>
+            </div>
+            <p className="ltr:font-[gabriola] rtl:font-kufam text-xl xl:text-[22px] rtl:text-base rtl:sm:text-lg rtl:xl:text-lg leading-5 ltr:lg:!leading-9 rtl:leading-10 rtl:lg:!leading-loose">
+              <FormattedMessage id="servicesBoxDescription" />
+            </p>
+            <button
+              type="button"
+              className="primary-btn bg-[#daa520] text-white px-5 py-2 text-lg rounded font-gabriola w-[139px] h-[46px] hover:bg-[#b8860b]"
+            >
+              <FormattedMessage id="servicesBoxButton" />
+            </button>
+          </div>
+        </div>
+
+        {/* Service 4 */}
+        <div className="flex items-start md:items-center flex-col  gap-y-6 sm:gap-y-12 md:gap-x-12 xl:gap-x-32 md:flex-row-reverse">
+          <img
+            className="w-[300px] h-[300px] sm:w-[500px] sm:h-[420px] md:w-[300px] md:h-[420px] lg:w-[400px] lg:h-[400px] xl:w-[450px] xl:h-[450px] !rounded-2xl object-contain sm:object-cover lg:object-contain"
+            src={services2}
+            alt="Hotel Reservation"
+          />
+          <div className="flex flex-col gap-y-3 text-mid-night-blue ">
+            <span className="text-4xl lg:text-5xl rtl:font-bold xl:text-[60px] font-[montserrat] rtl:font-[kufam] rtl:text-golden-dark/60 text-[#bd9c0080] font-[General]">
+              04
+            </span>
+            <div className="flex flex-col text-2xl lg:text-[30px] font-bold rtl:text-xl rtl:lg:text-[28px] xl:text-[36px] ltr:font-[cooper] rtl:font-kufam rtl:leading-9 lg:gap-y-4">
+              <h1>
+                <FormattedMessage id="servicesBox2Heading1" />
+              </h1>
+              <h1>
+                <FormattedMessage id="servicesBox2Heading2" />
+              </h1>
+            </div>
+            <p className="ltr:font-[gabriola] rtl:font-kufam text-xl xl:text-[22px] rtl:text-base rtl:sm:text-lg rtl:xl:text-lg leading-5 ltr:lg:!leading-9 rtl:leading-10 rtl:lg:!leading-loose">
+              <FormattedMessage id="servicesBoxDescription" />
+            </p>
+            <button
+              type="button"
+              className="primary-btn bg-[#daa520] text-white px-5 py-2 text-lg rounded font-gabriola w-[139px] h-[46px] hover:bg-[#b8860b]"
+            >
+              <FormattedMessage id="servicesBoxButton" />
+            </button>
+          </div>
+        </div>
+        {/* Service 5 */}
+        <div className="flex items-start md:items-center flex-col md:flex-row gap-y-6 sm:gap-y-12 md:gap-x-12 xl:gap-x-32">
+          <img
+            className="w-[300px] h-[300px] sm:w-[500px] sm:h-[420px] md:w-[300px] md:h-[420px] lg:w-[400px] lg:h-[400px] xl:w-[450px] xl:h-[450px] !rounded-2xl object-contain sm:object-cover lg:object-contain"
+            src={services1}
+            alt="International And Domestic Flight Tickets"
+          />
+          <div className="flex flex-col gap-y-3 text-mid-night-blue">
+            <span className="text-4xl lg:text-5xl rtl:font-bold xl:text-[60px] font-[montserrat] rtl:font-[kufam] rtl:text-golden-dark/60 text-[#bd9c0080] ">
+              05
+            </span>
+            <div className="flex flex-col text-2xl lg:text-[30px] font-bold rtl:text-xl rtl:lg:text-[28px] xl:text-[36px] ltr:font-[cooper] rtl:font-kufam rtl:leading-9 lg:gap-y-4">
+              <h1>
+                <FormattedMessage id="servicesBox1Heading1" />
+              </h1>
+              <h1>
+                <FormattedMessage id="servicesBox1Heading2" />
+              </h1>
+            </div>
+            <p className="ltr:font-[gabriola] rtl:font-kufam text-xl xl:text-[22px] rtl:text-base rtl:sm:text-lg rtl:xl:text-lg leading-5 ltr:lg:!leading-9 rtl:leading-10 rtl:lg:!leading-loose">
+              <FormattedMessage id="servicesBoxDescription" />
+            </p>
+            <button
+              type="button"
+              className="primary-btn bg-[#daa520] text-white px-5 py-2 text-lg rounded font-gabriola w-[139px] h-[46px] hover:bg-[#b8860b]"
+            >
+              <FormattedMessage id="servicesBoxButton" />
+            </button>
+          </div>
+        </div>
+
+        {/* Service 6 */}
+        <div className="flex items-start md:items-center flex-col  gap-y-6 sm:gap-y-12 md:gap-x-12 xl:gap-x-32 md:flex-row-reverse">
+          <img
+            className="w-[300px] h-[300px] sm:w-[500px] sm:h-[420px] md:w-[300px] md:h-[420px] lg:w-[400px] lg:h-[400px] xl:w-[450px] xl:h-[450px] !rounded-2xl object-contain sm:object-cover lg:object-contain"
+            src={services2}
+            alt="Hotel Reservation"
+          />
+          <div className="flex flex-col gap-y-3 text-mid-night-blue ">
+            <span className="text-4xl lg:text-5xl rtl:font-bold xl:text-[60px] font-[montserrat] rtl:font-[kufam] rtl:text-golden-dark/60 text-[#bd9c0080] font-[General]">
+              06
+            </span>
+            <div className="flex flex-col text-2xl lg:text-[30px] font-bold rtl:text-xl rtl:lg:text-[28px] xl:text-[36px] ltr:font-[cooper] rtl:font-kufam rtl:leading-9 lg:gap-y-4">
+              <h1>
+                <FormattedMessage id="servicesBox2Heading1" />
+              </h1>
+              <h1>
+                <FormattedMessage id="servicesBox2Heading2" />
+              </h1>
+            </div>
+            <p className="ltr:font-[gabriola] rtl:font-kufam text-xl xl:text-[22px] rtl:text-base rtl:sm:text-lg rtl:xl:text-lg leading-5 ltr:lg:!leading-9 rtl:leading-10 rtl:lg:!leading-loose">
+              <FormattedMessage id="servicesBoxDescription" />
+            </p>
+            <button
+              type="button"
+              className="primary-btn bg-[#daa520] text-white px-5 py-2 text-lg rounded font-gabriola w-[139px] h-[46px] hover:bg-[#b8860b]"
+            >
+              <FormattedMessage id="servicesBoxButton" />
+            </button>
+          </div>
+        </div>
+        {/* Service 7 */}
+        <div className="flex items-start md:items-center flex-col md:flex-row gap-y-6 sm:gap-y-12 md:gap-x-12 xl:gap-x-32">
+          <img
+            className="w-[300px] h-[300px] sm:w-[500px] sm:h-[420px] md:w-[300px] md:h-[420px] lg:w-[400px] lg:h-[400px] xl:w-[450px] xl:h-[450px] !rounded-2xl object-contain sm:object-cover lg:object-contain"
+            src={services1}
+            alt="International And Domestic Flight Tickets"
+          />
+          <div className="flex flex-col gap-y-3 text-mid-night-blue">
+            <span className="text-4xl lg:text-5xl rtl:font-bold xl:text-[60px] font-[montserrat] rtl:font-[kufam] rtl:text-golden-dark/60 text-[#bd9c0080] ">
+              07
+            </span>
+            <div className="flex flex-col text-2xl lg:text-[30px] font-bold rtl:text-xl rtl:lg:text-[28px] xl:text-[36px] ltr:font-[cooper] rtl:font-kufam rtl:leading-9 lg:gap-y-4">
+              <h1>
+                <FormattedMessage id="servicesBox1Heading1" />
+              </h1>
+              <h1>
+                <FormattedMessage id="servicesBox1Heading2" />
+              </h1>
+            </div>
+            <p className="ltr:font-[gabriola] rtl:font-kufam text-xl xl:text-[22px] rtl:text-base rtl:sm:text-lg rtl:xl:text-lg leading-5 ltr:lg:!leading-9 rtl:leading-10 rtl:lg:!leading-loose">
+              <FormattedMessage id="servicesBoxDescription" />
+            </p>
+            <button
+              type="button"
+              className="primary-btn bg-[#daa520] text-white px-5 py-2 text-lg rounded font-gabriola w-[139px] h-[46px] hover:bg-[#b8860b]"
+            >
+              <FormattedMessage id="servicesBoxButton" />
+            </button>
+          </div>
+        </div>
+
+        {/* Service 8 */}
+        <div className="flex items-start md:items-center flex-col  gap-y-6 sm:gap-y-12 md:gap-x-12 xl:gap-x-32 md:flex-row-reverse">
+          <img
+            className="w-[300px] h-[300px] sm:w-[500px] sm:h-[420px] md:w-[300px] md:h-[420px] lg:w-[400px] lg:h-[400px] xl:w-[450px] xl:h-[450px] !rounded-2xl object-contain sm:object-cover lg:object-contain"
+            src={services2}
+            alt="Hotel Reservation"
+          />
+          <div className="flex flex-col gap-y-3 text-mid-night-blue ">
+            <span className="text-4xl lg:text-5xl rtl:font-bold xl:text-[60px] font-[montserrat] rtl:font-[kufam] rtl:text-golden-dark/60 text-[#bd9c0080] font-[General]">
+              08
             </span>
             <div className="flex flex-col text-2xl lg:text-[30px] font-bold rtl:text-xl rtl:lg:text-[28px] xl:text-[36px] ltr:font-[cooper] rtl:font-kufam rtl:leading-9 lg:gap-y-4">
               <h1>
